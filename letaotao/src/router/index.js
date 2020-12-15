0940affe5ba10a78b45563ee383dd20079d1beef
deciduous73 <958675469@qq.com>
2020-12-15 11:37:05 +0800

    头部

--- letaotao/src/router/index.js
+++ letaotao/src/router/index.js
@@ -1,35 +1,32 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 const router = new VueRouter({
-    routes: [{
-            path: '/',
-            redirect: '/shouye'
-        },
-        {
-            path: '/shouye',
-            // redirect: '/shouye/nowplaying',
-            component: () =>
-                import ('../views/shouye/index'),
-        },
-        {
-            path: '/fenlei',
-            component: () =>
-                import ('../views/fenlei/index')
-        },
-        {
-            path: '/cart',
-            component: () =>
-                import ('../views/cart/index')
-        },
-        {
-            path: '/mine',
-            component: () =>
-                import ('../views/mine/index')
-        }
-    ],
-    linkActiveClass: "active"
-})
+  routes: [
+    {
+      path: "/",
+      redirect: "/shouye",
+    },
+    {
+      path: "/shouye",
+      // redirect: '/shouye/nowplaying',
+      component: () => import("../views/shouye/index.vue"),
+    },
+    {
+      path: "/fenlei",
+      component: () => import("../views/fenlei/index"),
+    },
+    {
+      path: "/cart",
+      component: () => import("../views/cart/index"),
+    },
+    {
+      path: "/mine",
+      component: () => import("../views/mine/index"),
+    },
+  ],
+  linkActiveClass: "active",
+});
 
-export default router
+export default router;
---
+++ letaotao/src/views/shouye/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="">
+    <Header></Header>
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img
+          v-lazy="image"
+          :style="{ width: 420 + 'px', height: 150 + 'px' }"
+        />
+      </van-swipe-item>
+    </van-swipe>
+
+    <van-row type="flex" justify="space-around" :style="{ fontSize: 3 + 'px' }">
+      <van-col span="6"><van-icon name="passed" />网易自营品牌</van-col>
+      <van-col span="6"><van-icon name="passed" />30天无忧退货</van-col>
+      <van-col span="6"><van-icon name="passed" />48小时快速退款</van-col>
+    </van-row>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Lazyload } from "vant";
+
+Vue.use(Lazyload);
+
+import { Col, Row } from "vant";
+
+Vue.use(Col);
+Vue.use(Row);
+
+import { Checkbox, CheckboxGroup } from "vant";
+
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+
+import Header from "@/components/header/index.vue";
+export default {
+  data() {
+    return {
+      images: [
+        "http://yanxuan.nosdn.127.net/aef1dfa6e25c61c96a8fcaa24e756b83.jpg?imageView&thumbnail=750x0&quality=75",
+        "http://yanxuan.nosdn.127.net/0260e7764f00606950ee0933ceadc84e.jpg?imageView&thumbnail=750x0&quality=75",
+        "http://yanxuan.nosdn.127.net/e6d195c3ed656777e0e34c142a496c88.jpg?imageView&thumbnail=750x0&quality=75",
+        "http://yanxuan.nosdn.127.net/41bf0751457a405e1137f01d66d7f843.jpg?imageView&thumbnail=750x0&quality=75",
+        "http://yanxuan.nosdn.127.net/574dafc21b52e225d80bd660dc4f8b99.jpg?imageView&thumbnail=750x0&quality=75",
+      ],
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style scoped></style>
