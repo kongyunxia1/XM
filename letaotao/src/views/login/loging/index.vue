7fc03fb43684ad104c13420b8303d169572ac296
kyx <l>
2020-12-16 09:11:26 +0800

    fjk

---
+++ letaotao/src/views/login/loging/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class='resgiest'>
+    <Header></Header>
+    <router-view></router-view>
+    <span class="wz" >网易严选</span><br>
+    <div class="form">
+      
+      <van-form>
+        <van-field
+          v-model="username"
+          name="手机号"
+          label="手机号"
+          placeholder="请填写手机号"
+          :rules="[{ required: true, message: '请填写手机号' }]"
+        />
+        <van-field
+          v-model="password"
+          type="password"
+          name="密码"
+          label="密码"
+          placeholder="密码"
+          :rules="[{ required: true, message: '请填写密码' }]"
+        />
+        <div style="margin: 16px;">
+          <van-button square block type="danger" native-type="submit" @click="gomine">
+            登录
+          </van-button>
+        </div>
+      </van-form>
+      <div class="res">
+        <input type="checkbox" v-model="checked">
+        <span>我同意<a href="#">《服务条款》</a>和<a href="">《网易隐私政策》</a></span>
+      </div>
+      <div class="gores">
+        <span>还没有账号，</span>
+        <router-link to="/resgiest" href="#">去注册 ></router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import Header from "../login/components/header/index"
+export default {
+  data() {
+    return {
+      checked: true,
+      username: '',
+      password: '',
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    gomine(){
+      axios.post("/api/login", {
+          userName: this.username,
+          password: this.password,
+        })
+        .then((res) => {
+          console.log(res)
+           if (res.data.code == 20000) {
+             alert(res.data.msg)
+              this.$router.push({
+                path: "/mine", 
+              });
+            }else{
+              alert(res.data.msg)
+            }
+          
+          
+        });
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style scoped>
+.wz{
+  font-size:30px;
+  margin-left:120px;
+}
+.form{
+  margin-top:80px;
+}
+.res{
+  margin-left: 10px;
+
+}
+.gores{
+  margin-top:40px;
+  margin-left: 100px;
+}
+</style>
