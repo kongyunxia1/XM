779e926906e639950d27375275d366400b072f80
Your Name <2902126001@qq.com>
2020-12-16 10:12:18 +0800

    123

--- letaotao/src/main.js
+++ letaotao/src/main.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-import axios from "axios"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store/index";
+import axios from "axios";
+import Vant from "vant";
+import "vant/lib/index.css";
 
-Vue.prototype.$http = axios
+Vue.use(Vant);
 
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
